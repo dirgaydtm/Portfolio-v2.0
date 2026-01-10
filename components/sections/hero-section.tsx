9df0132141d39b97e4271f0222e3405158a9fd07
Dirgaaa <dirgayuditama6@gmail.com>
2026-01-10 22:06:18 +0700

    feat: update profile pic component

--- components/sections/hero-section.tsx
+++ components/sections/hero-section.tsx
@@ -1,20 +1,18 @@
 "use client";
 
-import { motion } from "motion/react";
-import { MapPin, Download } from "lucide-react";
+import { Download } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
 import { profile } from "@/lib/data";
-import { scrollToSection, getInitials } from "@/lib/utils";
+import { scrollToSection, } from "@/lib/utils";
 import LetterGlitch from "@/components/blocks/letter-glitch";
+import { ProfilePicture } from "@/components/blocks/profile-picture";
 
 export function HeroSection() {
     return (
         <section
             id="about"
-            className="relative flex h-[80vh] flex-col items-center justify-center px-4 sm:px-6"
+            className="relative flex h-screen flex-col items-center justify-center px-4 sm:px-6"
         >
             {/* Background with glitch + overlay */}
             <div className="absolute inset-0 hidden h-100 dark:flex lg:h-200 z-0">
@@ -27,82 +25,19 @@ export function HeroSection() {
             </div>
 
             {/* Main content */}
-            <div className="z-1 mx-auto flex max-w-3xl flex-col items-center text-center ">
-                {/* Avatar + Badge */}
-                <div className="relative">
-                    <div className="relative h-32 w-32 overflow-hidden rounded-full shadow-xl md:h-72 md:w-72">
-                        <motion.div
-                            className="absolute inset-0"
-                            initial={{ opacity: 1 }}
-                            whileHover={{ opacity: 0 }}
-                            transition={{ duration: 0.3 }}
-                        >
-                            <Avatar className="h-full w-full">
-                                <AvatarImage src={profile.avatar} alt={profile.name} />
-                                <AvatarFallback className="text-2xl font-bold">
-                                    {getInitials(profile.name)}
-                                </AvatarFallback>
-                            </Avatar>
-                        </motion.div>
-                        <motion.div
-                            className="absolute inset-0"
-                            initial={{ opacity: 0 }}
-                            whileHover={{ opacity: 1 }}
-                            transition={{ duration: 0.3 }}
-                        >
-                            <Avatar className="h-full w-full">
-                                <AvatarImage src={profile.logo} alt={profile.name} />
-                                <AvatarFallback className="text-2xl font-bold">
-                                    {getInitials(profile.name)}
-                                </AvatarFallback>
-                            </Avatar>
-                        </motion.div>
-                    </div>
-
-
-                    {profile.availableForWork && (
-                        <Badge
-                            variant="secondary"
-                            className="
-                absolute bottom-0 left-1/2 -translate-x-1/2
-                gap-2 text-xs sm:text-sm
-                bg-emerald-100 text-emerald-700
-                dark:bg-emerald-950 dark:text-emerald-400
-              "
-                        >
-                            <span className="relative flex h-2 w-2">
-                                <span className="absolute h-full w-full animate-ping rounded-full bg-emerald-500 opacity-75" />
-                                <span className="relative h-2 w-2 rounded-full bg-emerald-500" />
-                            </span>
-                            Available for work
-                        </Badge>
-                    )}
-                </div>
-
-                {/* Name & Title */}
-                <h1 className="mt-6 text-3xl font-bold tracking-tight text-foreground md:text-4xl">
-                    {profile.name}
-                </h1>
-
-                <p className="mt-1 text-xl font-medium text-muted-foreground">
-                    {profile.title}
-                </p>
-
-                {/* Location */}
-                <div className="mt-2 flex items-center gap-2 text-muted-foreground">
-                    <MapPin className="h-4 w-4" />
-                    <span>{profile.location}</span>
-                </div>
-
-                <Separator className="my-6 w-24" />
+            <div className="z-1 mx-auto flex gap-6 max-w-3xl flex-col items-center">
+                {/* Profile Picture */}
+                <ProfilePicture />
 
                 {/* Bio */}
-                <p className="max-w-xl text-md leading-relaxed text-muted-foreground">
+                <p className="max-w-xl text-md text-center leading-relaxed text-muted-foreground">
                     {profile.bio}
                 </p>
 
+                <Separator className="w-full" />
+
                 {/* CTA + Socials */}
-                <div className="mt-6 flex flex-wrap items-center justify-center gap-4 gap-y-2">
+                <div className="flex flex-wrap items-center justify-center gap-4 gap-y-2">
                     <Button size="lg" onClick={() => scrollToSection("contact")}>
                         Get in Touch
                     </Button>
